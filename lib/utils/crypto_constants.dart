7eb53637a1f7ab4afc1117f9779777f3636b93c2
Akash Nigam <akashnigam010@gmail.com>
2020-11-07 19:00:16 +0530

    fixed untracked files

--- lib/utils/crypto_constants.dart
+++
@@ -1,6 +0,0 @@
-class CryptoConstants {
-  // list of integers for salt - actual salt not checked in github
-  static final List<int> saltNumList = [1, 2, 3];
-  // iv can have max length if 16 chars - actual iv not checked in github
-  static final String iv = '****************';
-}
