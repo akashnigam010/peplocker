8b7227f24d601b1fcffbf7df2114e8e32ec150f8
Akash Nigam <akashnigam010@gmail.com>
2020-11-10 15:04:34 +0530

    Added firebase crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -8,7 +9,9 @@ import 'package:peplocker/utils/app_colors.dart';
 import 'package:peplocker/utils/constants.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
